ce18e13144c427efc3af7b470c20edb463322649
Paul PANGANIBAN <ps.panganiban@gmail.com>
2016-12-16 09:14:11 +0100

    Add quotation marks for configuration name

--- plugin/sar.drone/src-gen/sar/drone/drn/impl/ConfigurationImpl.java
+++ plugin/sar.drone/src-gen/sar/drone/drn/impl/ConfigurationImpl.java
@@ -379,7 +379,7 @@ public class ConfigurationImpl extends RootImpl implements Configuration
     if (eIsProxy()) return super.toString();
 
     StringBuffer result = new StringBuffer();
-    result.append("<config name="+name+">");
+    result.append("<config name=\""+name+"\">");
     result.append("<connection>"+connection+"</connection>");
     result.append("<ip>"+ip+"</ip>");
     result.append("</config>");
